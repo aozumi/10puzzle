747505f897dfc02f74d31bfaaffa7ce5a018ff75
aozumi <68194841+aozumi@users.noreply.github.com>
2021-04-30 18:15:57 +0900

    New function subsums

--- test_utils.py
+++ test_utils.py
@@ -1,4 +1,5 @@
-from utils import first
+from utils import first, subsums
+
 
 def test_first__empty() -> None:
     assert first([], "empty") == "empty"
@@ -6,3 +7,10 @@ def test_first__empty() -> None:
 
 def test_first() -> None:
     assert first([1, 2, 3], None) == 1
+
+
+def test_subsums() -> None:
+    assert list(subsums([])) == []
+    assert list(subsums([1])) == [1]
+    assert list(subsums([1, 2])) == [1, 2, 3]
+    assert list(subsums([1, 2, 4])) == [1, 2, 4, 3, 5, 6, 7]
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
-from typing import Iterable, TypeVar, Union, cast
+from typing import Generator, Iterable, Sequence, TypeVar, Union, cast
+from itertools import combinations
 
 
 T = TypeVar('T')
@@ -8,3 +9,14 @@ S = TypeVar('S')
 def first(xs: Iterable[T], default: S = None) -> Union[T, S]:
     "xs の最初の要素を返す。xs が空の場合は default を返す。"
     return next(iter(xs), cast(S, default))
+
+
+def subsums(values: Sequence[T]) -> Generator[T, None, None]:
+    """
+    valuesの部分和を全て生成する。
+    """
+    for size in range(1, len(values) + 1):
+        for subset in combinations(values, size):
+            # sum: Union[T, int] だが、int は引数が空のケース(0を返す)。
+            # subset は空ではないので、T にキャストして問題ない。
+            yield cast(T, sum(subset))
